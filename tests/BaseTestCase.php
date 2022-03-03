2835624866ddb7ce7c5f033b36299b52005adbc8
Diego Calle <dnetix@gmail.com>
2022-03-03 14:14:36 -0500

    Add basic service creation for testing purposes

--- tests/BaseTestCase.php
+++ tests/BaseTestCase.php
@@ -3,9 +3,21 @@
 namespace Tests;
 
 use PHPUnit\Framework\TestCase;
+use PlacetoPay\Kount\Helpers\MockClient;
+use PlacetoPay\Kount\KountService;
 
 class BaseTestCase extends TestCase
 {
+    public function service(array $overrides = []): KountService
+    {
+        return new KountService(array_merge([
+            'client' => MockClient::client(),
+            'merchant' => getenv('MERCHANT') ?: 'YOUR_MERCHANT',
+            'apiKey' => getenv('APIKEY') ?: 'THE_API_KEY_PROVIDED',
+            'website' => getenv('WEBSITE') ?: 'THE_AWC_SITE_NAME',
+        ], $overrides));
+    }
+
     public function serialize($data)
     {
         return base64_encode(serialize($data));
