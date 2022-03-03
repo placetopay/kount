949362c396892c51263dbdbe452a45caf5c8ecdf
Diego Calle <dnetix@gmail.com>
2022-03-03 14:15:29 -0500

    Add static creation for the exception

--- src/Exceptions/KountServiceException.php
+++ src/Exceptions/KountServiceException.php
@@ -4,4 +4,12 @@ namespace PlacetoPay\Kount\Exceptions;
 
 class KountServiceException extends \Exception
 {
+    public static function forErrorResponse(array $data): self
+    {
+        $messages = [];
+        for ($i = 0; $i < $data['ERROR_COUNT']; $i++) {
+            $messages[] = $data['ERROR_' . $i];
+        }
+        return new self(implode("\n", $messages));
+    }
 }
