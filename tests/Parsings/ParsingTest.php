95ba573cfa4a96ef2afa425935bd34645701fca1
Gabriela Muñoz Nupan <gabriela.munoz@evertecinc.com>
2025-02-10 16:48:04 -0500

    test: add test the resquest without phone or mobile

--- tests/Parsings/ParsingTest.php
+++ tests/Parsings/ParsingTest.php
@@ -263,4 +263,250 @@ class ParsingTest extends BaseTestCase
         $inquiryRequest = $this->service->parseInquiryRequest(5, $data)->asRequestData();
         $this->assertEquals(190000, $inquiryRequest['TOTL']);
     }
+    
+    public function testItParsesTheInquiryRequestWithoutPhone(): void
+    {
+        $data = $this->basicRequestData([
+            'payer' => [
+                'address' => [
+                    'street' => 'Fake street 123',
+                    'city' => 'Medellin',
+                    'state' => 'Antioquia',
+                    'postalCode' => '050012',
+                    'country' => 'CO',
+                ],
+            ],
+            'payment' => [
+                'amount' => [
+                    'total' => 12300,
+                ],
+                'items' => [
+                    [
+                        'sku' => '111',
+                        'name' => 'TV Sony 32',
+                        'category' => 'physical',
+                        'qty' => '1',
+                        'price' => '2340',
+                        'tax' => '300',
+                    ],
+                    [
+                        'sku' => '234',
+                        'name' => 'Wireless Mouse',
+                        'category' => 'physical',
+                        'qty' => '2',
+                        'price' => '543',
+                        'tax' => '56',
+                    ],
+                ],
+                'shipping' => [
+                    'name' => 'Diego',
+                    'surname' => 'Calle',
+                    'email' => 'dnetix@gmail.com',
+                    'address' => [
+                        'street' => 'Fake street 321',
+                        'city' => 'Sabaneta',
+                        'state' => 'Antioquia',
+                        'postalCode' => '050013',
+                        'country' => 'CO',
+                        'phone' => '4442310',
+                    ],
+                ],
+            ],
+            'cardNumber' => '4111111111111111',
+            'gender' => 'M',
+            'additional' => [
+                'key_1' => 'Some Value 1',
+            ],
+            'shipmentType' => \PlacetoPay\Kount\Messages\Request::SHIP_SAME,
+        ]);
+
+        $inquiryRequest = $this->service->parseInquiryRequest('123', $data);
+
+        $requestData = [
+            'VERS' => '0630',
+            'MODE' => 'Q',
+            'SDK' => 'PHP',
+            'SDK_VERSION' => 'PlacetoPay-0.0.1',
+
+            'MERC' => '201000',
+            'SESS' => '123',
+            'ORDR' => $data['payment']['reference'],
+            'CURR' => $data['payment']['amount']['currency'],
+            'TOTL' => 1230000,
+            'MACK' => $data['mack'],
+
+            'PTYP' => 'CARD',
+            'LAST4' => substr($data['cardNumber'], -4),
+            'PTOK' => '411111XXXXXX1111',
+            'CVVR' => $data['cvvStatus'],
+            'CCMM' => '12',
+            'CCYY' => '2020',
+
+            'UNIQ' => $data['payer']['documentType'] . $data['payer']['document'],
+
+            'NAME' => $data['payer']['name'] . ' ' . $data['payer']['surname'],
+            'GENDER' => $data['gender'],
+            'EMAL' => $data['payer']['email'],
+            'B2A1' => $data['payer']['address']['street'],
+            'B2CI' => $data['payer']['address']['city'],
+            'B2ST' => $data['payer']['address']['state'],
+            'B2PC' => $data['payer']['address']['postalCode'],
+            'B2CC' => $data['payer']['address']['country'],
+            'B2PN' => $data['payer']['mobile'],
+
+            'S2NM' => $data['payment']['shipping']['name'] . ' ' . $data['payment']['shipping']['surname'],
+            'S2EM' => $data['payment']['shipping']['email'],
+            'S2A1' => $data['payment']['shipping']['address']['street'],
+            'S2CI' => $data['payment']['shipping']['address']['city'],
+            'S2ST' => $data['payment']['shipping']['address']['state'],
+            'S2PC' => $data['payment']['shipping']['address']['postalCode'],
+            'S2CC' => $data['payment']['shipping']['address']['country'],
+            'S2PN' => $data['payment']['shipping']['address']['phone'],
+
+            'UDF[KEY_1]' => $data['additional']['key_1'],
+
+            'PROD_TYPE[0]' => $data['payment']['items'][0]['sku'],
+            'PROD_ITEM[0]' => $data['payment']['items'][0]['name'],
+            'PROD_QUANT[0]' => $data['payment']['items'][0]['qty'],
+            'PROD_PRICE[0]' => 234000,
+            'PROD_TYPE[1]' => $data['payment']['items'][1]['sku'],
+            'PROD_ITEM[1]' => $data['payment']['items'][1]['name'],
+            'PROD_QUANT[1]' => $data['payment']['items'][1]['qty'],
+            'PROD_PRICE[1]' => 54300,
+
+            'IPAD' => $data['ipAddress'],
+            'UAGT' => $data['userAgent'],
+            'SITE' => 'DEFAULT',
+            'PENC' => 'MASK',
+        ];
+
+        $this->assertEquals($requestData, $inquiryRequest->asRequestData(), 'Parses the inquiry data correctly');
+        $this->assertEquals([
+            'X-Kount-Api-Key' => 'TESTING',
+        ], $inquiryRequest->asRequestHeaders(), 'Parses the inquiry headers correctly');
+    }
+
+    public function testItParsesTheInquiryRequestWithoutPhoneOrMobile(): void
+    {
+        $data = $this->basicRequestData([
+            'payer' => [
+                'address' => [
+                    'street' => 'Fake street 123',
+                    'city' => 'Medellin',
+                    'state' => 'Antioquia',
+                    'postalCode' => '050012',
+                    'country' => 'CO',
+                ],
+            ],
+            'payment' => [
+                'amount' => [
+                    'total' => 12300,
+                ],
+                'items' => [
+                    [
+                        'sku' => '111',
+                        'name' => 'TV Sony 32',
+                        'category' => 'physical',
+                        'qty' => '1',
+                        'price' => '2340',
+                        'tax' => '300',
+                    ],
+                    [
+                        'sku' => '234',
+                        'name' => 'Wireless Mouse',
+                        'category' => 'physical',
+                        'qty' => '2',
+                        'price' => '543',
+                        'tax' => '56',
+                    ],
+                ],
+                'shipping' => [
+                    'name' => 'Diego',
+                    'surname' => 'Calle',
+                    'email' => 'dnetix@gmail.com',
+                    'address' => [
+                        'street' => 'Fake street 321',
+                        'city' => 'Sabaneta',
+                        'state' => 'Antioquia',
+                        'postalCode' => '050013',
+                        'country' => 'CO',
+                        'phone' => '4442310',
+                    ],
+                ],
+            ],
+            'cardNumber' => '4111111111111111',
+            'gender' => 'M',
+            'additional' => [
+                'key_1' => 'Some Value 1',
+            ],
+            'shipmentType' => \PlacetoPay\Kount\Messages\Request::SHIP_SAME,
+        ]);
+
+        unset($data['payer']['mobile']);
+
+        $inquiryRequest = $this->service->parseInquiryRequest('123', $data);
+
+        $requestData = [
+            'VERS' => '0630',
+            'MODE' => 'Q',
+            'SDK' => 'PHP',
+            'SDK_VERSION' => 'PlacetoPay-0.0.1',
+
+            'MERC' => '201000',
+            'SESS' => '123',
+            'ORDR' => $data['payment']['reference'],
+            'CURR' => $data['payment']['amount']['currency'],
+            'TOTL' => 1230000,
+            'MACK' => $data['mack'],
+
+            'PTYP' => 'CARD',
+            'LAST4' => substr($data['cardNumber'], -4),
+            'PTOK' => '411111XXXXXX1111',
+            'CVVR' => $data['cvvStatus'],
+            'CCMM' => '12',
+            'CCYY' => '2020',
+
+            'UNIQ' => $data['payer']['documentType'] . $data['payer']['document'],
+
+            'NAME' => $data['payer']['name'] . ' ' . $data['payer']['surname'],
+            'GENDER' => $data['gender'],
+            'EMAL' => $data['payer']['email'],
+            'B2A1' => $data['payer']['address']['street'],
+            'B2CI' => $data['payer']['address']['city'],
+            'B2ST' => $data['payer']['address']['state'],
+            'B2PC' => $data['payer']['address']['postalCode'],
+            'B2CC' => $data['payer']['address']['country'],
+            'B2PN' => null,
+
+            'S2NM' => $data['payment']['shipping']['name'] . ' ' . $data['payment']['shipping']['surname'],
+            'S2EM' => $data['payment']['shipping']['email'],
+            'S2A1' => $data['payment']['shipping']['address']['street'],
+            'S2CI' => $data['payment']['shipping']['address']['city'],
+            'S2ST' => $data['payment']['shipping']['address']['state'],
+            'S2PC' => $data['payment']['shipping']['address']['postalCode'],
+            'S2CC' => $data['payment']['shipping']['address']['country'],
+            'S2PN' => $data['payment']['shipping']['address']['phone'],
+
+            'UDF[KEY_1]' => $data['additional']['key_1'],
+
+            'PROD_TYPE[0]' => $data['payment']['items'][0]['sku'],
+            'PROD_ITEM[0]' => $data['payment']['items'][0]['name'],
+            'PROD_QUANT[0]' => $data['payment']['items'][0]['qty'],
+            'PROD_PRICE[0]' => 234000,
+            'PROD_TYPE[1]' => $data['payment']['items'][1]['sku'],
+            'PROD_ITEM[1]' => $data['payment']['items'][1]['name'],
+            'PROD_QUANT[1]' => $data['payment']['items'][1]['qty'],
+            'PROD_PRICE[1]' => 54300,
+
+            'IPAD' => $data['ipAddress'],
+            'UAGT' => $data['userAgent'],
+            'SITE' => 'DEFAULT',
+            'PENC' => 'MASK',
+        ];
+
+        $this->assertEquals($requestData, $inquiryRequest->asRequestData(), 'Parses the inquiry data correctly');
+        $this->assertEquals([
+            'X-Kount-Api-Key' => 'TESTING',
+        ], $inquiryRequest->asRequestHeaders(), 'Parses the inquiry headers correctly');
+    }
 }
